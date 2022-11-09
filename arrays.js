e8f647020ffcd4f68c88e978e914e4208f9faefd
Kirk Hauck <kirkihauck@gmail.com>
2022-11-09 15:49:30 -0700

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,10 +1,19 @@
 // Mod 0 Session 3 Arrays CFU
+// ==========================
 
-// Declare a variable that stores an Array of at least 4 Strings.
 var strings = ["alpha", "beta", "gamma", "delta"]
-
-// Declare a variable that stores an Array of at least 4 Numbers.
 var numbers = [0, 1, 2, 3]
-
-// Declare a variable that stores an Array of at least 4 Booleans.
 var booleans = [true, false, true, false]
+
+// .push() will add new variable(s) I designate within () and add it to the end of the array
+strings.push("epsilon")
+console.log(strings)
+
+// .unshift() will add new variable(s) I designate within () and add it to the start of the array
+numbers.unshift(-2, -1)
+console.log(numbers)
+
+
+booleans.pop()
+console.log(booleans)
+// .pop() will remove the final variable in the array

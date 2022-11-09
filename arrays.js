bf52b4ae03d53c0296ed1c90bd727e21c63e84fb
Kirk Hauck <kirkihauck@gmail.com>
2022-11-09 15:36:29 -0700

    Add arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+// Mod 0 Session 3 Arrays CFU
+
+// Declare a variable that stores an Array of at least 4 Strings.
+var strings = ["alpha", "beta", "gamma", "delta"]
+
+// Declare a variable that stores an Array of at least 4 Numbers.
+var numbers = [0, 1, 2, 3]
+
+// Declare a variable that stores an Array of at least 4 Booleans.
+var booleans = [true, false, true, false]
